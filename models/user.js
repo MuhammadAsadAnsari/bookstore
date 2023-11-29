788ab8ef9888235d7f23d0ff004feccace07a002
Me_Asad <muhammadasaad561@gmail.com>
2023-11-29 15:40:24 +0500

    Password hashing

--- models/user.js
+++ models/user.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
-const {isEmail}= require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        required : [true, 'Please enter an email'],
-        unique : true,
-        lowercase:true,
-        validate : [isEmail,'Please enter a valid email']
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
-    password : {type:String,required:[true,'Please enter a password'],minlength:[6,'Minimum password length is 6']}, 
+    password: { type: String, required: [true, 'Please enter a password'], minlength: [6, 'Minimum password length is 6'] },
 })
-userSchema.post('save',(doc,next)=>{
-    console.log("new user was created",doc);
-   next();
-})
-userSchema.pre('save',(next)=>{
-    console.log("user about to be created",this);
+userSchema.post('save', (doc, next) => {
+    console.log("new user was created", doc);
     next();
 })
-const User = mongoose.model('user',userSchema)
-module.exports =  User;
+userSchema.pre('save', async (next) => {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    console.log("user about to be created", this);
+    next();
+})
+const User = mongoose.model('user', userSchema)
+module.exports = User;
